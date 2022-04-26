fa59a28183c2827c35ffb70d6aa30fa5ebbf19b2
umarov90 <ramzan.umarov@riken.jp>
2022-04-26 21:21:02 +0900

    Init

---
+++ utils.py
@@ -0,0 +1,14 @@
+enc_mat = np.append(np.eye(4),
+                    [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0],
+                     [0, 0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0],
+                     [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]], axis=0)
+enc_mat = enc_mat.astype(np.bool)
+mapping_pos = dict(zip("ACGTRYSWKMBDHVN", range(15)))
+
+def encode_seq(seq):
+    try:
+        seq2 = [mapping_pos[i] for i in seq]
+        return enc_mat[seq2]
+    except:
+        print(seq)
+        return None
